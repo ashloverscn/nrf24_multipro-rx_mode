ce8870f854f5a64937026ffd5c62b2d9f7a336b5
ashloverscn <ashloverscn@gmail.com>
2022-08-24 04:29:39 +0530

    Add files via upload

---
+++ nRF24_multipro_MOD_3servo_4buttons/Bayang.ino
@@ -0,0 +1,307 @@
+/*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License.
+ If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BAYANG_BIND_COUNT       1000
+#define BAYANG_PACKET_PERIOD    2000
+#define BAYANG_PACKET_SIZE      15
+#define BAYANG_RF_NUM_CHANNELS  4
+#define BAYANG_RF_BIND_CHANNEL  0
+#define BAYANG_ADDRESS_LENGTH   5
+
+static uint8_t Bayang_rf_chan = 0;
+static uint8_t Bayang_rf_channels[BAYANG_RF_NUM_CHANNELS] = {0,};
+static uint8_t Bayang_rx_tx_addr[BAYANG_ADDRESS_LENGTH];
+
+enum{
+    // flags going to packet[2]
+    BAYANG_FLAG_RTH      = 0x01,
+    BAYANG_FLAG_HEADLESS = 0x02,
+    BAYANG_FLAG_FLIP     = 0x08,
+    BAYANG_FLAG_VIDEO    = 0x10,
+    BAYANG_FLAG_SNAPSHOT = 0x20,
+};
+
+enum{
+    // flags going to packet[3]
+    BAYANG_FLAG_INVERT   = 0x80,
+};
+
+uint32_t process_Bayang()
+{
+    uint32_t timeout = micros() + BAYANG_PACKET_PERIOD;
+#ifndef RX_MODE
+    Bayang_send_packet(0);
+#else
+    // timeout = micros() + BAYANG_PACKET_PERIOD/10;
+    Bayang_recv_packet();
+#endif
+    return timeout;
+}
+
+void Bayang_init()
+{
+    const u8 bind_address[] = {0,0,0,0,0};
+#ifndef RX_MODE
+    uint8_t i;
+    for(i=0; i<BAYANG_ADDRESS_LENGTH; i++) {
+        Bayang_rx_tx_addr[i] = random() & 0xff;
+    }
+    Bayang_rf_channels[0] = 0x00;
+    for(i=1; i<BAYANG_RF_NUM_CHANNELS; i++) {
+        Bayang_rf_channels[i] = random() % 0x42;
+    }
+#endif
+    NRF24L01_Initialize();
+#ifndef RX_MODE
+    NRF24L01_SetTxRxMode(TX_EN);
+#else
+    NRF24L01_SetTxRxMode(RX_EN);
+#endif
+    XN297_SetTXAddr(bind_address, BAYANG_ADDRESS_LENGTH);
+#ifdef RX_MODE
+    XN297_SetRXAddr(bind_address, BAYANG_ADDRESS_LENGTH);
+#endif
+    NRF24L01_FlushTx();
+    NRF24L01_FlushRx();
+    NRF24L01_WriteReg(NRF24L01_01_EN_AA, 0x00);      // No Auto Acknowldgement on all data pipes
+    NRF24L01_WriteReg(NRF24L01_02_EN_RXADDR, 0x01);
+#ifdef RX_MODE
+    NRF24L01_WriteReg(NRF24L01_11_RX_PW_P0, BAYANG_PACKET_SIZE); // rx pipe 0
+#endif
+    NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, 0x03);   // address size
+    NRF24L01_WriteReg(NRF24L01_04_SETUP_RETR, 0x00); // no retransmits
+    NRF24L01_SetBitrate(NRF24L01_BR_1M);             // 1Mbps
+    NRF24L01_SetPower(RF_POWER);
+    NRF24L01_Activate(0x73);                         // Activate feature register
+    NRF24L01_WriteReg(NRF24L01_1C_DYNPD, 0x00);      // Disable dynamic payload length on all pipes
+    NRF24L01_WriteReg(NRF24L01_1D_FEATURE, 0x01);
+    NRF24L01_Activate(0x73);
+    delay(150);
+
+#ifdef RX_MODE
+    XN297_Configure(_BV(NRF24L01_00_EN_CRC) | _BV(NRF24L01_00_CRCO) | _BV(NRF24L01_00_PWR_UP) | _BV(NRF24L01_00_PRIM_RX));
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+    NRF24L01_FlushRx();
+#endif
+}
+
+void Bayang_bind()
+{
+#ifndef RX_MODE
+    uint16_t counter = BAYANG_BIND_COUNT;
+    while(counter) {
+        Bayang_send_packet(1);
+        delayMicroseconds(BAYANG_PACKET_PERIOD);
+        digitalWrite(ledPin, counter-- & 0x10);
+    }
+    XN297_SetTXAddr(Bayang_rx_tx_addr, BAYANG_ADDRESS_LENGTH);
+    digitalWrite(ledPin, HIGH);
+#else
+    Bayang_bind_rx();
+#endif
+}
+
+void Bayang_bind_rx()
+{
+    int bind_count = 0;
+    uint8_t bind_packet[BAYANG_PACKET_SIZE] = {0};
+
+    uint32_t timeout;
+
+    digitalWrite(ledPin, LOW);
+
+    NRF24L01_WriteReg(NRF24L01_05_RF_CH, BAYANG_RF_BIND_CHANNEL);
+
+    while(bind_count < 10) {
+        timeout = millis()+5;
+
+        while(millis()<timeout) {
+            delay(1);
+            if(NRF24L01_ReadReg(NRF24L01_07_STATUS) & _BV(NRF24L01_07_RX_DR)) { // data received from tx
+                XN297_ReadPayload(packet, BAYANG_PACKET_SIZE);
+
+                NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+                NRF24L01_FlushRx();
+
+                if( packet[0] == 0xA4)
+                {
+                  if (0 == bind_count)
+                  {
+                    memcpy(bind_packet, packet, BAYANG_PACKET_SIZE);
+                    ++bind_count;
+                  }
+                  else
+                  {
+                    if (0 == memcmp(bind_packet, packet, BAYANG_PACKET_SIZE))
+                      ++bind_count;
+                  }
+                }
+                break;
+            }
+        }
+    }
+
+    memcpy(Bayang_rx_tx_addr, &packet[1], 5);
+    memcpy(Bayang_rf_channels, &packet[6], 4);
+    transmitterID[0] = packet[10];
+    transmitterID[1] = packet[11];
+
+    XN297_SetTXAddr(Bayang_rx_tx_addr, BAYANG_ADDRESS_LENGTH);
+    XN297_SetRXAddr(Bayang_rx_tx_addr, BAYANG_ADDRESS_LENGTH);
+
+    NRF24L01_WriteReg(NRF24L01_05_RF_CH, Bayang_rf_channels[Bayang_rf_chan++]);
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+    NRF24L01_FlushRx();
+    digitalWrite(ledPin, HIGH);
+}
+
+#define DYNTRIM(chval) ((u8)((chval >> 2) & 0xfc))
+void Bayang_recv_packet()
+{
+  static bool is_bound = false;
+  static uint16_t failsafe_counter = 0;
+  if(NRF24L01_ReadReg(NRF24L01_07_STATUS) & _BV(NRF24L01_07_RX_DR)) { // data received from tx
+    is_bound = true;
+    failsafe_counter = 0;
+
+    int sum = 0;
+    uint16_t roll, pitch, yaw, throttle, output1, output2, output3, output4;
+    XN297_ReadPayload(packet, BAYANG_PACKET_SIZE);
+
+    if( packet[0] == 0xA4)
+    {
+      // bind packet
+      Serial.println("bind packet");
+    }
+    else if (packet[0] == 0xA5)
+    {
+      // data packet
+      for(int i=0; i<14; i++)
+      {
+        sum += packet[i];
+      }
+      if ( (sum&0xFF) == packet[14] )
+      {
+        // checksum OK
+        roll = (packet[4] & 0x0003) * 256 + packet[5];
+        pitch = (packet[6] & 0x0003) * 256 + packet[7];
+        yaw = (packet[10] & 0x0003) * 256 + packet[11];
+        throttle = (packet[8] & 0x0003) * 256 + packet[9];
+        output1 = (packet[2] & 0x08);
+        output2 = (packet[2] & 0x01);
+        output3 = (packet[2] & 0x02);
+        output4 = (packet[3] & 0x80);
+        
+        // Serial.print(roll, DEC); Serial.print(",");
+        // Serial.print(pitch, DEC); Serial.print(",");
+        // Serial.print(yaw, DEC); Serial.print(",");
+        // Serial.print(throttle, DEC); Serial.println("");
+
+        // roll: 0 .. 1023; servo 0 .. 180 but stop at +-80� due to reduced holding current
+        servo1.write( - asin( roll / 511.5 - 1.0 ) / ( 3.1416 / 2 ) * 80.0 + 90 );
+		    servo2.write( pitch / 1023.0 * 180 );
+        servo3.write( yaw / 1023.0 * 180 );
+        // throttle: 0 .. 1023; pwm 0 .. 255
+        analogWrite( 6, throttle / 4  );
+        digitalWrite ( 10, output1 );
+        digitalWrite ( 11, output2 );
+        digitalWrite ( 12, output3 );
+        digitalWrite ( 13, output4 );
+      }
+      else
+      {
+        //checksum FAIL
+        Serial.println("checksum FAIL");
+      }
+      NRF24L01_WriteReg(NRF24L01_05_RF_CH, Bayang_rf_channels[Bayang_rf_chan++]);
+      Bayang_rf_chan %= sizeof(Bayang_rf_channels);
+      NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+      NRF24L01_FlushRx();
+    }
+    else
+    {
+        Serial.print("Unrecognized packet: ");
+        Serial.println(packet[0], HEX);
+    }
+  } else if ( is_bound ) {
+        ++failsafe_counter;
+        if ( failsafe_counter > 1000 ) {
+            is_bound = false;
+            failsafe_counter = 0;
+            // neutral steering position; motor off:
+            servo1.write( 90 );
+            servo2.write( 90 );
+            servo3.write( 90 );
+            analogWrite( 6, 0 );
+            // enable rebinding:
+            extern bool reset;
+            reset = true;
+            Serial.println("reset");
+        }
+  }
+}
+
+void Bayang_send_packet(u8 bind)
+{
+    union {
+        u16 value;
+        struct {
+            u8 lsb;
+            u8 msb;
+        } bytes;
+    } chanval;
+
+    if (bind) {
+        packet[0] = 0xa4;
+        memcpy(&packet[1], Bayang_rx_tx_addr, 5);
+        memcpy(&packet[6], Bayang_rf_channels, 4);
+        packet[10] = transmitterID[0];
+        packet[11] = transmitterID[1];
+    } else {
+        packet[0] = 0xa5;
+        packet[1] = 0xfa;   // normal mode is 0xf7, expert 0xfa
+        packet[2] = GET_FLAG(AUX2, BAYANG_FLAG_FLIP)
+                  | GET_FLAG(AUX5, BAYANG_FLAG_HEADLESS)
+                  | GET_FLAG(AUX6, BAYANG_FLAG_RTH)
+                  | GET_FLAG(AUX3, BAYANG_FLAG_SNAPSHOT)
+                  | GET_FLAG(AUX4, BAYANG_FLAG_VIDEO);
+        packet[3] = GET_FLAG(AUX1, BAYANG_FLAG_INVERT);
+        chanval.value = map(ppm[AILERON], PPM_MIN, PPM_MAX, 0, 0x3ff);   // aileron
+        packet[4] = chanval.bytes.msb + DYNTRIM(chanval.value);
+        packet[5] = chanval.bytes.lsb;
+        chanval.value = map(ppm[ELEVATOR], PPM_MIN, PPM_MAX, 0, 0x3ff);   // elevator
+        packet[6] = chanval.bytes.msb + DYNTRIM(chanval.value);
+        packet[7] = chanval.bytes.lsb;
+        chanval.value = map(ppm[THROTTLE], PPM_MIN, PPM_MAX, 0, 0x3ff);   // throttle
+        packet[8] = chanval.bytes.msb + 0x7c;
+        packet[9] = chanval.bytes.lsb;
+        chanval.value = map(ppm[RUDDER], PPM_MIN, PPM_MAX, 0, 0x3ff);   // rudder
+        packet[10] = chanval.bytes.msb + DYNTRIM(chanval.value);
+        packet[11] = chanval.bytes.lsb;
+    }
+    packet[12] = transmitterID[2];
+    packet[13] = 0x0a;
+    packet[14] = 0;
+    for(uint8_t i=0; i<BAYANG_PACKET_SIZE-1; i++) {
+        packet[14] += packet[i];
+    }
+
+    XN297_Configure(_BV(NRF24L01_00_EN_CRC) | _BV(NRF24L01_00_CRCO) | _BV(NRF24L01_00_PWR_UP));
+    NRF24L01_WriteReg(NRF24L01_05_RF_CH, bind ? BAYANG_RF_BIND_CHANNEL : Bayang_rf_channels[Bayang_rf_chan++]);
+    Bayang_rf_chan %= sizeof(Bayang_rf_channels);
+    NRF24L01_WriteReg(NRF24L01_07_STATUS, 0x70);
+    NRF24L01_FlushTx();
+    XN297_WritePayload(packet, BAYANG_PACKET_SIZE);
+}
---
+++ nRF24_multipro_MOD_3servo_4buttons/nRF24_multipro.ino
@@ -0,0 +1,434 @@
+/*
+ ##########################################
+ #####   MultiProtocol nRF24L01 Tx   ######
+ ##########################################
+ #        by goebish on rcgroups          #
+ #                                        #
+ #   Parts of this project are derived    #
+ #     from existing work, thanks to:     #
+ #                                        #
+ #   - PhracturedBlue for DeviationTX     #
+ #   - victzh for XN297 emulation layer   #
+ #   - Hasi for Arduino PPM decoder       #
+ #   - hexfet, midelic, closedsink ...    #
+ ##########################################
+
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License.
+ If not, see <http://www.gnu.org/licenses/>.
+
+ 2016.sep.25
+ Added:
+ Servo1 - D7
+ Servo2 - D8
+ Servo3 - D9
+ On/Off from "FLIP" button      - D10
+ On/Off from "RETURN" button    - D11
+ On/Off from "HEADLESS" button  - D12
+ On/Off from "INVERT" button    - D13
+ */
+
+#include <util/atomic.h>
+#include <EEPROM.h>
+#include "iface_nrf24l01.h"
+
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+// uncomment the below define to use nRF24_multipro as a receiver
+// instead of a transmitter. you must also explicity set the
+// 'current_protocol' variable below the the protocol you wish
+// the receiver to use.
+// Currently only the following RX protocols have been implemented:
+// PROTO_BAYANG
+#define RX_MODE
+
+// ############ Wiring ################
+#define PPM_pin   2  // PPM in
+//SPI Comm.pins with nRF24L01
+#define MOSI_pin  3  // MOSI - D3
+#define SCK_pin   4  // SCK  - D4
+#define CE_pin    5  // CE   - D5
+#define MISO_pin  A0 // MISO - A0
+#define CS_pin    A1 // CS   - A1
+
+#define ledPin    A2 // LED  - A2
+
+// SPI outputs
+#define MOSI_on PORTD |= _BV(3)  // PD3
+#define MOSI_off PORTD &= ~_BV(3)// PD3
+#define SCK_on PORTD |= _BV(4)   // PD4
+#define SCK_off PORTD &= ~_BV(4) // PD4
+#define CE_on PORTD |= _BV(5)    // PD5
+#define CE_off PORTD &= ~_BV(5)  // PD5
+#define CS_on PORTC |= _BV(1)    // PC1
+#define CS_off PORTC &= ~_BV(1)  // PC1
+// SPI input
+#define  MISO_on (PINC & _BV(0)) // PC0
+
+#define RF_POWER TX_POWER_80mW
+
+// PPM stream settings
+#define CHANNELS 12 // number of channels in ppm stream, 12 ideally
+enum chan_order{
+    THROTTLE,
+    AILERON,
+    ELEVATOR,
+    RUDDER,
+    AUX1,  // (CH5)  led light, or 3 pos. rate on CX-10, H7, or inverted flight on H101
+    AUX2,  // (CH6)  flip control
+    AUX3,  // (CH7)  still camera (snapshot)
+    AUX4,  // (CH8)  video camera
+    AUX5,  // (CH9)  headless
+    AUX6,  // (CH10) calibrate Y (V2x2), pitch trim (H7), RTH (Bayang, H20), 360deg flip mode (H8-3D, H22)
+    AUX7,  // (CH11) calibrate X (V2x2), roll trim (H7)
+    AUX8,  // (CH12) Reset / Rebind
+};
+
+#define PPM_MIN 1000
+#define PPM_SAFE_THROTTLE 1050
+#define PPM_MID 1500
+#define PPM_MAX 2000
+#define PPM_MIN_COMMAND 1300
+#define PPM_MAX_COMMAND 1700
+#define GET_FLAG(ch, mask) (ppm[ch] > PPM_MAX_COMMAND ? mask : 0)
+#define GET_FLAG_INV(ch, mask) (ppm[ch] < PPM_MIN_COMMAND ? mask : 0)
+
+// supported protocols
+enum {
+    PROTO_V2X2 = 0,     // WLToys V2x2, JXD JD38x, JD39x, JJRC H6C, Yizhan Tarantula X6 ...
+    PROTO_CG023,        // EAchine CG023, CG032, 3D X4
+    PROTO_CX10_BLUE,    // Cheerson CX-10 blue board, newer red board, CX-10A, CX-10C, Floureon FX-10, CX-Stars (todo: add DM007 variant)
+    PROTO_CX10_GREEN,   // Cheerson CX-10 green board
+    PROTO_H7,           // EAchine H7, MoonTop M99xx
+    PROTO_BAYANG,       // EAchine H8(C) mini, H10, BayangToys X6, X7, X9, JJRC JJ850, Floureon H101
+    PROTO_SYMAX5C1,     // Syma X5C-1 (not older X5C), X11, X11C, X12
+    PROTO_YD829,        // YD-829, YD-829C, YD-822 ...
+    PROTO_H8_3D,        // EAchine H8 mini 3D, JJRC H20, H22
+    PROTO_MJX,          // MJX X600 (can be changed to Weilihua WLH08, X800 or H26D)
+    PROTO_SYMAXOLD,     // Syma X5C, X2
+    PROTO_HISKY,        // HiSky RXs, HFP80, HCP80/100, FBL70/80/90/100, FF120, HMX120, WLToys v933/944/955 ...
+    PROTO_KN,           // KN (WLToys variant) V930/931/939/966/977/988
+    PROTO_YD717,        // Cheerson CX-10 red (older version)/CX11/CX205/CX30, JXD389/390/391/393, SH6057/6043/6044/6046/6047, FY326Q7, WLToys v252 Pro/v343, XinXun X28/X30/X33/X39/X40
+    PROTO_END
+};
+
+// EEPROM locationss
+enum{
+    ee_PROTOCOL_ID = 0,
+    ee_TXID0,
+    ee_TXID1,
+    ee_TXID2,
+    ee_TXID3
+};
+
+uint8_t transmitterID[4];
+uint8_t current_protocol = PROTO_BAYANG;
+static volatile bool ppm_ok = false;
+uint8_t packet[32];
+static bool reset=true;
+volatile uint16_t Servo_data[12];
+static uint16_t ppm[12] = {PPM_MIN,PPM_MIN,PPM_MIN,PPM_MIN,PPM_MID,PPM_MID,
+                           PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,PPM_MID,};
+
+void setup()
+{
+    randomSeed((analogRead(A4) & 0x1F) | (analogRead(A5) << 5));
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW); //start LED off
+    pinMode(PPM_pin, INPUT);
+    pinMode(MOSI_pin, OUTPUT);
+    pinMode(SCK_pin, OUTPUT);
+    pinMode(CS_pin, OUTPUT);
+    pinMode(CE_pin, OUTPUT);
+    pinMode(MISO_pin, INPUT);
+
+#ifndef RX_MODE
+    // PPM ISR setup
+    attachInterrupt(PPM_pin - 2, ISR_ppm, CHANGE);
+    TCCR1A = 0;  //reset timer1
+    TCCR1B = 0;
+    TCCR1B |= (1 << CS11);  //set timer1 to increment every 1 us @ 8MHz, 0.5 us @16MHz
+
+#endif
+    set_txid(false);
+
+    Serial.begin( 115200 );
+    Serial.println( "Start" );
+    servo1.attach( 7 );
+	  servo2.attach( 8 );
+    servo3.attach( 9 );
+    pinMode( 6, OUTPUT );
+    pinMode( 10, OUTPUT );
+    pinMode( 11, OUTPUT );
+    pinMode( 12, OUTPUT );
+    pinMode( 13, OUTPUT );
+}
+
+void loop()
+{
+    uint32_t timeout=0;
+    // reset / rebind
+#ifndef RX_MODE
+    if(reset || ppm[AUX8] > PPM_MAX_COMMAND) {
+#else
+    if(reset) {
+#endif
+        reset = false;
+#ifndef RX_MODE
+        selectProtocol();
+#endif
+        NRF24L01_Reset();
+        NRF24L01_Initialize();
+        init_protocol();
+    }
+    // process protocol
+    switch(current_protocol) {
+        case PROTO_CG023:
+        case PROTO_YD829:
+            timeout = process_CG023();
+            break;
+        case PROTO_V2X2:
+            timeout = process_V2x2();
+            break;
+        case PROTO_CX10_GREEN:
+        case PROTO_CX10_BLUE:
+            timeout = process_CX10();
+            break;
+        case PROTO_H7:
+            timeout = process_H7();
+            break;
+        case PROTO_BAYANG:
+            timeout = process_Bayang();
+            break;
+        case PROTO_SYMAX5C1:
+        case PROTO_SYMAXOLD:
+            timeout = process_SymaX();
+            break;
+        case PROTO_H8_3D:
+            timeout = process_H8_3D();
+            break;
+        case PROTO_MJX:
+            timeout = process_MJX();
+            break;
+        case PROTO_HISKY:
+            timeout = process_HiSky();
+            break;
+        case PROTO_KN:
+            timeout = process_KN();
+            break;
+        case PROTO_YD717:
+            timeout = process_YD717();
+            break;
+    }
+#ifndef RX_MODE
+    // updates ppm values out of ISR
+    update_ppm();
+#endif
+    // wait before sending next packet
+    while(micros() < timeout)
+    {   };
+}
+
+void set_txid(bool renew)
+{
+    uint8_t i;
+    for(i=0; i<4; i++)
+        transmitterID[i] = EEPROM.read(ee_TXID0+i);
+    if(renew || (transmitterID[0]==0xFF && transmitterID[1]==0x0FF)) {
+        for(i=0; i<4; i++) {
+            transmitterID[i] = random() & 0xFF;
+            EEPROM.update(ee_TXID0+i, transmitterID[i]);
+        }
+    }
+}
+
+void selectProtocol()
+{
+    // wait for multiple complete ppm frames
+    ppm_ok = false;
+    uint8_t count = 10;
+    while(count) {
+        while(!ppm_ok) {} // wait
+        update_ppm();
+        if(ppm[AUX8] < PPM_MAX_COMMAND) // reset chan released
+            count--;
+        ppm_ok = false;
+    }
+
+    // startup stick commands
+
+    if(ppm[RUDDER] < PPM_MIN_COMMAND)        // Rudder left
+        set_txid(true);                      // Renew Transmitter ID
+
+    // protocol selection
+
+    // Rudder right + Aileron left + Elevator up
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[AILERON] < PPM_MIN_COMMAND && ppm[ELEVATOR] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_YD717; // Cheerson CX-10 red (older version)/CX11/CX205/CX30, JXD389/390/391/393, SH6057/6043/6044/6046/6047, FY326Q7, WLToys v252 Pro/v343, XinXun X28/X30/X33/X39/X40
+
+    // Rudder right + Aileron left + Elevator down
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[AILERON] < PPM_MIN_COMMAND && ppm[ELEVATOR] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_KN; // KN (WLToys variant) V930/931/939/966/977/988
+
+    // Rudder right + Elevator down
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[ELEVATOR] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_HISKY; // HiSky RXs, HFP80, HCP80/100, FBL70/80/90/100, FF120, HMX120, WLToys v933/944/955 ...
+
+    // Rudder right + Elevator up
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[ELEVATOR] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_SYMAXOLD; // Syma X5C, X2 ...
+
+    // Rudder right + Aileron right
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[AILERON] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_MJX; // MJX X600, other sub protocols can be set in code
+
+    // Rudder right + Aileron left
+    else if(ppm[RUDDER] > PPM_MAX_COMMAND && ppm[AILERON] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_H8_3D; // H8 mini 3D, H20 ...
+
+    // Elevator down + Aileron right
+    else if(ppm[ELEVATOR] < PPM_MIN_COMMAND && ppm[AILERON] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_YD829; // YD-829, YD-829C, YD-822 ...
+
+    // Elevator down + Aileron left
+    else if(ppm[ELEVATOR] < PPM_MIN_COMMAND && ppm[AILERON] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_SYMAX5C1; // Syma X5C-1, X11, X11C, X12
+
+    // Elevator up + Aileron right
+    else if(ppm[ELEVATOR] > PPM_MAX_COMMAND && ppm[AILERON] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_BAYANG;    // EAchine H8(C) mini, BayangToys X6/X7/X9, JJRC JJ850 ...
+
+    // Elevator up + Aileron left
+    else if(ppm[ELEVATOR] > PPM_MAX_COMMAND && ppm[AILERON] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_H7;        // EAchine H7, MT99xx
+
+    // Elevator up
+    else if(ppm[ELEVATOR] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_V2X2;       // WLToys V202/252/272, JXD 385/388, JJRC H6C ...
+
+    // Elevator down
+    else if(ppm[ELEVATOR] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_CG023;      // EAchine CG023/CG031/3D X4, (todo :ATTOP YD-836/YD-836C) ...
+
+    // Aileron right
+    else if(ppm[AILERON] > PPM_MAX_COMMAND)
+        current_protocol = PROTO_CX10_BLUE;  // Cheerson CX10(blue pcb, newer red pcb)/CX10-A/CX11/CX12 ...
+
+    // Aileron left
+    else if(ppm[AILERON] < PPM_MIN_COMMAND)
+        current_protocol = PROTO_CX10_GREEN;  // Cheerson CX10(green pcb)...
+
+    // read last used protocol from eeprom
+    else
+        current_protocol = constrain(EEPROM.read(ee_PROTOCOL_ID),0,PROTO_END-1);
+    // update eeprom
+    EEPROM.update(ee_PROTOCOL_ID, current_protocol);
+    // wait for safe throttle
+    while(ppm[THROTTLE] > PPM_SAFE_THROTTLE) {
+        delay(100);
+        update_ppm();
+    }
+}
+
+void init_protocol()
+{
+    switch(current_protocol) {
+        case PROTO_CG023:
+        case PROTO_YD829:
+            CG023_init();
+            CG023_bind();
+            break;
+        case PROTO_V2X2:
+            V2x2_init();
+            V2x2_bind();
+            break;
+        case PROTO_CX10_GREEN:
+        case PROTO_CX10_BLUE:
+            CX10_init();
+            CX10_bind();
+            break;
+        case PROTO_H7:
+            H7_init();
+            H7_bind();
+            break;
+        case PROTO_BAYANG:
+            Bayang_init();
+            Bayang_bind();
+            break;
+        case PROTO_SYMAX5C1:
+        case PROTO_SYMAXOLD:
+            Symax_init();
+            break;
+        case PROTO_H8_3D:
+            H8_3D_init();
+            H8_3D_bind();
+            break;
+        case PROTO_MJX:
+            MJX_init();
+            MJX_bind();
+            break;
+        case PROTO_HISKY:
+            HiSky_init();
+            break;
+        case PROTO_KN:
+            kn_start_tx(true); // autobind
+            break;
+        case PROTO_YD717:
+            YD717_init();
+            break;
+    }
+}
+
+// update ppm values out of ISR
+void update_ppm()
+{
+    for(uint8_t ch=0; ch<CHANNELS; ch++) {
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+            ppm[ch] = Servo_data[ch];
+        }
+    }
+}
+
+void ISR_ppm()
+{
+    #if F_CPU == 16000000
+        #define PPM_SCALE 1L
+    #elif F_CPU == 8000000
+        #define PPM_SCALE 0L
+    #else
+        #error // 8 or 16MHz only !
+    #endif
+    static unsigned int pulse;
+    static unsigned long counterPPM;
+    static byte chan;
+    counterPPM = TCNT1;
+    TCNT1 = 0;
+    ppm_ok=false;
+    if(counterPPM < 510 << PPM_SCALE) {  //must be a pulse if less than 510us
+        pulse = counterPPM;
+    }
+    else if(counterPPM > 1910 << PPM_SCALE) {  //sync pulses over 1910us
+        chan = 0;
+    }
+    else{  //servo values between 510us and 2420us will end up here
+        if(chan < CHANNELS) {
+            Servo_data[chan]= constrain((counterPPM + pulse) >> PPM_SCALE, PPM_MIN, PPM_MAX);
+            if(chan==3)
+                ppm_ok = true; // 4 first channels Ok
+        }
+        chan++;
+    }
+}
